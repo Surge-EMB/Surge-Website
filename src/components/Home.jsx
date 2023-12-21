1027ea6eeb29ecc64aef572ad72eec55cca3f24b
cameronvdang <cameronvdang@gmail.com>
2023-12-21 04:13:34 -0800

    homepage bg opacity update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -78,7 +78,7 @@ const Home = () => {
                 style={{ backgroundImage: `url(${backgroundImage})` }}
             >
                 {/* Dark Overlay */}
-                <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                <div className="absolute inset-0 bg-black bg-opacity-70"></div>
                 <div className="absolute inset-0 backdrop-blur-md"></div>
 
                 <RevealOnScroll>
