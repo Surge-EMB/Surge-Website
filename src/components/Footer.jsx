c634304d34687d4f39e8de4e74934492de1a7048
cameronvdang <cameronvdang@gmail.com>
2024-01-16 18:45:42 -0800

    fixed footer text color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-950 p-4 mt-8">
+    <footer className="bg-slate-950 p-4 mt-8 text-slate-100">
         <div className="container mx-auto text-center">
             <b>Email:</b> surge@berkeley.edu | <b>Instagram:</b> <a className="underline hover:font-bold" href="https://www.instagram.com/surge.emb">@surge.emb</a> | <b>LinkedIn:</b> <a className="underline hover:font-bold" href="https://www.linkedin.com/company/surge-emb/about/">Surge-EMB</a>
         </div>
